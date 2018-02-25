c8b99742bcd6b34a5e4aff641e30d9b2060d6eae
Dom <domjt@me.com>
2018-02-25 20:10:03 +0000

    refacter(react-dom) Use hydrate rather than render

--- src/client.jsx
+++ src/client.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { hydrate } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import { Root } from './components';
 
-render((
+hydrate((
   <BrowserRouter>
     <Root />
   </BrowserRouter>
