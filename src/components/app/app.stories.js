d5ee1c67440db23d45b43ce38784648c52baf372
Dom <domjt@me.com>
2018-03-10 22:37:58 +0000

    chore(storybook): Added component README

--- src/components/app/app.stories.js
+++ src/components/app/app.stories.js
@@ -1,8 +1,12 @@
 import { storiesOf } from '@storybook/react';
+import { withReadme } from 'storybook-readme';
 
+import readme from './README.md';
 import stories from './stories';
 
 
 const buttonStories = storiesOf('App', module);
+
+buttonStories.addDecorator(withReadme(readme));
 
 stories.forEach(({ name, story }) => buttonStories.add(name, story));
