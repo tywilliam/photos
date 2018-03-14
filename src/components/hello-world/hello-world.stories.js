ba6993b2be0c455531ba24bd2cba2d9135e4104b
Dom <domjt@me.com>
2018-03-14 22:46:41 +0000

    chore(hello-world): Renamed stories array

--- src/components/hello-world/hello-world.stories.js
+++ src/components/hello-world/hello-world.stories.js
@@ -5,8 +5,8 @@ import readme from './README.md';
 import stories from './stories';
 
 
-const buttonStories = storiesOf('HelloWorld', module);
+const componentStories = storiesOf('HelloWorld', module);
 
-buttonStories.addDecorator(withReadme(readme));
+componentStories.addDecorator(withReadme(readme));
 
-stories.forEach(({ name, story }) => buttonStories.add(name, story));
+stories.forEach(({ name, story }) => componentStories.add(name, story));
