fd00621d71c9dbdd5e520e0bf79d22a2ca506e62
Dom <domjt@me.com>
2018-03-22 22:02:48 +0000

    test(picture): Stubbed tesets

--- src/components/picture/picture.test.jsx
+++ src/components/picture/picture.test.jsx
@@ -1,15 +1,37 @@
+import Enzyme, { render, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import * as React from 'react';
-import renderer from 'react-test-renderer';
 
 import Picture from './';
+import Source from '../source';
 
-describe('Picture', () => {
-  test('It renders', () => {
-    const component = renderer.create(
-      <Picture />,
-    );
+Enzyme.configure({ adapter: new Adapter() });
 
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+describe('<Picture />', () => {
+  xit('always renders a picture', () => {
+    const wrapper = render(<Picture />);
+    expect(wrapper.find('picture')).toHaveLength(1);
+  });
+
+  it('always renders one <img /> tag', () => {
+    const wrapper = render(<Picture />);
+    expect(wrapper.find('img')).toHaveLength(1);
+  });
+
+  xit('only renders <Source /> as children', () => {
+    // const wrapper = shallow((
+    //   <Picture>
+    //     <div className="unique" />
+    //   </Picture>
+    // ));
+
+    const wrapper = shallow((
+      <Picture>
+        <Source />
+      </Picture>
+    ));
+
+    // expect(pictureWithInvalidChild.contains(<div className="unique" />)).toBeFalsy();
+    expect(wrapper.find(Source)).toHaveLength(1);
   });
 });
--- src/components/source/index.js
+++ src/components/source/index.js
@@ -1,1 +1,1 @@
-export default from './source';
+export * as source from './source';
