19a103de9aab5fa95c62582b9423432b1cda148b
Dom <domjt@me.com>
2018-03-24 14:19:59 +0000

    test(picture): Removed stubbed test

--- src/components/picture/picture.test.jsx
+++ src/components/picture/picture.test.jsx
@@ -8,11 +8,6 @@ import Source from '../source';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Picture />', () => {
-  xit('always renders a picture', () => {
-    const wrapper = render(<Picture />);
-    expect(wrapper.find('picture')).toHaveLength(1);
-  });
-
   it('always renders one <img /> tag', () => {
     const wrapper = render(<Picture />);
     expect(wrapper.find('img')).toHaveLength(1);
