674f78f18b6ea2538db1370bd22064e639272e08
Dom <domjt@me.com>
2018-04-10 22:02:07 +0100

    refactor(routes): Updated react route params

--- src/router/express/routes/react.jsx
+++ src/router/express/routes/react.jsx
@@ -16,15 +16,15 @@ import { cssAssets, hash, jsAssets } from '../../../utils/webpack-stats';
 
 const router = new Router();
 
-router.get('*', (req, res) => {
+router.get('*', ({ url }, { status }) => {
   const match = routes.reduce((acc, route) => (
-    matchPath(req.url, route, {
+    matchPath(url, route, {
       exact: route.extact,
     }) || acc
   ), null);
 
   if (!match) {
-    res.status(NOT_FOUND).send('Not found');
+    status(NOT_FOUND).send('Not found');
     return;
   }
 
@@ -34,7 +34,7 @@ router.get('*', (req, res) => {
   const markup = renderToString(stylesheet.collectStyles(
     <StaticRouter
       context={context}
-      location={req.url}
+      location={url}
     >
       <App />
     </StaticRouter>,
@@ -44,7 +44,7 @@ router.get('*', (req, res) => {
   const manifestPath: string = join(__dirname, jsAssets.find(({ name }) => name === 'manifest').localPath);
   const manifest = readFileSync(manifestPath);
 
-  res.status(OK).render('react', {
+  status(OK).render('react', {
     jsassets: jsAssets.filter(({ name }) => name !== 'manifest'),
     cssassets: cssAssets,
     css,
