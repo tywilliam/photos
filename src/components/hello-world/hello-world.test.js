fc0fae3de71227c6481419d9572b16ccb732004c
Dom <domjt@me.com>
2018-04-19 22:03:31 +0100

    refactor(helloWorld): Removed

--- src/components/hello-world/hello-world.test.js
+++
@@ -1,5 +0,0 @@
-describe('HelloWorld', () => {
-  test('basic', () => {
-    expect(2).toBe(2);
-  });
-});
--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,10 @@
-import HelloWorld from './hello-world';
 import Picture from './picture';
 import Source from './source';
 
-export HelloWorld from './hello-world';
 export Picture from './picture';
 export Source from './source';
 
 export default {
-  HelloWorld,
   Picture,
   Source,
 };
