3a404f5584f7f054b392e5fa7e130a651cb92efa
Dom <domjt@me.com>
2018-06-26 21:22:00 +0100

    chore(picture): Update basic story img url

--- src/components/picture/stories/basic.jsx
+++ src/components/picture/stories/basic.jsx
@@ -8,7 +8,7 @@ const name = 'Basic';
 const story = () => (
   <Picture
     alt="This is alt text"
-    src="https://c1.staticflickr.com/5/4705/26691259268_9123358b35_n.jpg"
+    src="https://placeimg.com/1920/1080/nature"
     title="This is a title"
   />
 );
